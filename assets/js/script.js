f7565b9cebbc2174f02dfafd3e335271b1dffa9c
wildarte <wildarte10@gmail.com>
2022-09-22 20:47:37 -0300

    versao mobile da home

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ const cta_top = document.querySelector('.cta_top');
 const item_ask = document.querySelectorAll('.list_ask li h4');
 const item_ask_p = document.querySelectorAll('.list_ask li p');
 const icon_item = document.querySelectorAll('.list_ask li h4 i');
+const btn_menu = document.querySelector('.btn_menu');
+const nav_menu = document.querySelector('nav.menu');
+const btn_close_menu = document.querySelector('.btn_close_menu');
 
 $(document).ready(function(){
     $("#slide_hero").owlCarousel({
@@ -24,11 +27,32 @@ $(document).ready(function(){
         }
     });
 
+    $("#office_slide").owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        autoplayTimeout:1500,
+        callbacks: true,
+        transitionStyle: 'backSlide',
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
     $("#client_slide").owlCarousel({
         loop:true,
         margin:10,
         autoplay:true,
-        autoplayTimeout:2000,
+        autoplayTimeout: 8000,
         autoplayHoverPause:true,
         responsive:{
             0:{
@@ -39,6 +63,27 @@ $(document).ready(function(){
             },
             1000:{
                 items:3
+            }
+        }
+    });
+
+    $("#follow_right").owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2,
+                nav: true
+            },
+            1000:{
+                items:3,
+                nav: true
             }
         }
     });
@@ -55,21 +100,37 @@ item_ask.forEach((item, index) => {
     item.addEventListener('click', (e) => {
 
         if(item_ask_p[index]){
+
+            
             
             if(item_ask_p[index].classList.contains('open_list_p')){
 
                 item_ask_p[index].classList.remove('open_list_p');
                 icon_item[index].classList.replace('bi-plus','bi-dash');
+                
+                item_ask.forEach((item) => {
+                    item.classList.remove('bg-blue-dark');
+                    item.classList.remove('color-blue-dark');
+                    
+                })
     
             }else{
-                item_ask_p.forEach((item) => {
+                item_ask_p.forEach((item2) => {
     
-                    item.classList.remove('open_list_p');
+                    item2.classList.remove('open_list_p');
         
                 });
+
+                item_ask.forEach((item) => {
+                    item.classList.remove('bg-blue-dark');
+                    item.classList.remove('color-blue-dark');
+                    
+                })
         
                 item_ask_p[index].classList.add('open_list_p');
                 icon_item[index].classList.replace('bi-dash','bi-plus');
+                item.classList.add('bg-blue-dark');
+                item.classList.add('color-blue-dark');
         
             }
         }
@@ -77,3 +138,16 @@ item_ask.forEach((item, index) => {
     });
 
 })
+
+
+btn_menu.addEventListener('click', () => {
+
+    nav_menu.classList.add('open_menu_mobile');
+
+});
+
+btn_close_menu.addEventListener('click', () => {
+
+    nav_menu.classList.remove('open_menu_mobile');
+
+});
